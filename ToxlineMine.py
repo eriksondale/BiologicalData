54c5dce1527cbcba977c6c23aef64b1552f9ef24
Dale Erikson <dje23@pitt.edu>
2018-05-17 11:44:10 -0400

    Fully extracts data desired

--- ToxlineMine.py
+++ ToxlineMine.py
@@ -2,56 +2,56 @@ import urllib2
 import math
 from sys  import argv as argument
 
-def expandResulsArr(pageNum, tempFile): # Helper method meant to increase results size upon user request
-    print(pageNum)
+def expandResults(hits, tempFile): # Helper method meant to increase results size upon user request
     returnIDs = []
-    for x in range(0,pageNum):
+    for x in range(0, int(hits)):
         contents = urllib2.urlopen("https://toxgate.nlm.nih.gov/cgi-bin/sis/search2/g?" + tempFile + ":" +str(x)).read()
-        print(contents)
         IDStr = contents[contents.find("<Id>")+4:contents.find("</Id>")+(-1)]
         IDsTemp = IDStr.split(" ")
-        print(len(IDsTemp))
         for label in IDsTemp:
             returnIDs.append(label)
-    #remove duplicates
+    #remove duplicates from list
     finalIDs = []
     for label in returnIDs:
         if label not in finalIDs:
             finalIDs.append(label)
     #return statement
+    #returnIDs.sort()
     return finalIDs
 
 print('This script will query and return based on keywords entered....')
-keyword = argument[0]
+keyword = raw_input("Enter search words: ")
 keyword = keyword.replace(" ","+")
 
 contents = urllib2.urlopen("https://toxgate.nlm.nih.gov/cgi-bin/sis/search2/x?dbs+toxline:" + keyword).read()
 
 hits = contents[contents.find("<Count>")+7:contents.find("</Count>")]
+print(hits + " Hits")
+
 tempFile = contents[contents.find("<TemporaryFile>")+15:contents.find("</TemporaryFile>")]
-IDStr = contents[contents.find("<Id>")+4:contents.find("</Id>")+(-1)]
-IDs = IDStr.split(" ")
+print(tempFile + " TempLink")
+
 pageNum = int(math.ceil(int(hits)/ 10.0))
-
-print('There are ' + str(pageNum) + ' pages')
+print(str(pageNum) + ' Pages')
 print(' ')
-#answer = raw_input(hits + ' results were found. Do you want to list them all? (y/n)')
-#if(answer == "y"):
-IDs = expandResulsArr(pageNum, tempFile)
-    #for label in moreIDs:
-print(IDs)
-#else:
-    #print(IDs)
 
-'''
+answer = raw_input(hits + ' results were found. Do you want to list them all? (y/n)')
+if(answer == "y"):
+    IDs = expandResults(hits, tempFile)
+else:
+    IDStr = contents[contents.find("<Id>")+4:contents.find("</Id>")+(-1)]
+    IDs = IDStr.split(" ")
+print(IDs)
+
+# Printing the desired contents of the file
 for label in IDs:
     print(' ')
     print('==============================================')
     print(' ')
     data = urllib2.urlopen("https://toxgate.nlm.nih.gov/cgi-bin/sis/search2/z?dbs+toxline:@term+@DOCNO+" + label).read()
+    # Insert confirming keywords present here 
     title = data[data.find("<na>")+4:data.find("</na>")]
     abstract = data[data.find("<ab>")+4:data.find("</ab>")]
     print(title)
     print(' ')
     print(abstract)
-'''
