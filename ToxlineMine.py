6cf37706c24edf996747b847707d66e50db086eb
Dale Erikson <dje23@pitt.edu>
2018-05-21 11:41:03 -0400

    Added python-docx output

--- ToxlineMine.py
+++ ToxlineMine.py
@@ -1,6 +1,7 @@
 import urllib2
 import math
 from sys  import argv as argument
+from docx import Document
 
 def expandResults(hits, tempFile): # Helper method meant to increase results size upon user request
     returnIDs = []
@@ -21,19 +22,38 @@ def expandResults(hits, tempFile): # Helper method meant to increase results siz
 
 print('This script will query and return based on keywords entered....')
 keyword = raw_input("Enter search words: ")
-keyword = keyword.replace(" ","+")
 
+# Making list of keywords in order to bold in results later
+keywordTemp = keyword
+keywordTemp = keywordTemp.replace("(","")
+keywordTemp = keywordTemp.replace(")","")
+keywordTemp = keywordTemp.replace("AND","")
+keywordTemp = keywordTemp.replace("OR","")
+keywordTemp = keywordTemp.replace("NOT","")
+keyList = keywordTemp.split(" ")
+while '' in keyList:
+    keyList.remove('')
+
+document = Document()
+
+p = document.add_paragraph('')
+p.add_run('Toxline Search Query: ').bold = True
+p.add_run(keyword)
+
+keyword = keyword.replace(" ","+")
 contents = urllib2.urlopen("https://toxgate.nlm.nih.gov/cgi-bin/sis/search2/x?dbs+toxline:" + keyword).read()
 
 hits = contents[contents.find("<Count>")+7:contents.find("</Count>")]
-print(hits + " Hits")
+p = document.add_paragraph('')
+p.add_run('Hits: ').bold = True
+p.add_run(hits)
 
 tempFile = contents[contents.find("<TemporaryFile>")+15:contents.find("</TemporaryFile>")]
-print(tempFile + " TempLink")
+#print(tempFile + " TempLink")
 
 pageNum = int(math.ceil(int(hits)/ 10.0))
-print(str(pageNum) + ' Pages')
-print(' ')
+#print(str(pageNum) + ' Pages')
+#print(' ')
 
 answer = raw_input(hits + ' results were found. Do you want to list them all? (y/n)')
 if(answer == "y"):
@@ -41,30 +61,33 @@ if(answer == "y"):
 else:
     IDStr = contents[contents.find("<Id>")+4:contents.find("</Id>")+(-1)]
     IDs = IDStr.split(" ")
-print(IDs)
+#print(IDs)
 
 drugName = raw_input("What drug did you want?: ")
 count = 0
 
-print(' ')
-print('==============================================')
-print(' ')
+p.add_run('Drug: ').bold = True
+p.add_run(drugName.capitalize())
+
+document.add_paragraph(' ')
 # Printing the desired contents of the file
 for label in IDs:
     data = urllib2.urlopen("https://toxgate.nlm.nih.gov/cgi-bin/sis/search2/z?dbs+toxline:@term+@DOCNO+" + label).read()
     # Insert confirming keywords present here
-    if(drugName in data or drugName.title() in data):
+    if(drugName in data or drugName.capitalize() in data):
         count = count + 1
         title = data[data.find("<na>")+4:data.find("</na>")]
         authors = data[data.find("<au>")+4:data.find("</au>")]
         source =  data[data.find("<so>")+4:data.find("</so>")]
         abstract = data[data.find("<ab>")+4:data.find("</ab>")]
-        print(title)
-        print(authors)
-        print(source)
-        print(' ')
-        print(abstract)
-        print(' ')
-        print('==============================================')
-        print(' ')
-print(str(count) + " articles were found with the drug" )
+        p = document.add_paragraph(' ')
+        p = document.add_paragraph()
+        p.add_run(title).bold = True
+        p = document.add_paragraph()
+        p.add_run(authors)
+        p = document.add_paragraph()
+        p.add_run(source)
+        p = document.add_paragraph()
+        p.add_run(abstract)
+document.save(drugName.capitalize() + 'ToxlineSearch.docx')
+print(str(count) + " articles were found with the drug. See created document..." )
